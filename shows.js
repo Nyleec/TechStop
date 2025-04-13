37b9746dc9b57180774387c23dd9ebc63ef771a7
Nyleec <nyleecp@gmail.com>
2025-04-13 12:17:05 -0400

    cleaned up

--- shows.js
+++ shows.js
@@ -1,24 +1,34 @@
-const shows = ["https://dhzuxxxhixmwv.cloudfront.net/videoplayback.mp4","https://dhzuxxxhixmwv.cloudfront.net/TheDrive-ftHOME-Resonance.mp4"];
-const numShows= shows.length;
-var src = "";
+const videos = ["videoplayback.mp4","TheDrive-ftHOME-Resonance.mp4","Drive2"];
+const numVids= videos.length;
 
+function addElement() {
 
-function addElement([]) {
-
-    for( let i =0; i<numShows; i++){
-        src = shows[i];
+    for( let i =0; i<numVids; i++){
 
         const newDiv = document.createElement("video");
-        newDiv.src = src;
-        newDiv.autoplay = false;
+        newDiv.src = "https://dhzuxxxhixmwv.cloudfront.net/" + videos[i];
         newDiv.controls = true;
 
         const currentDiv = document.getElementById("video");
         document.body.insertBefore(newDiv, currentDiv);
     
-        console.log("element added");
+        console.log("element added, URL =" + videos[i]);
     }
 
   }
 
-addElement(shows);
+addElement();
+
+/* Search button code only for videos
+function search(string){
+var term = string;
+
+for (let i = 0; i <numVids; i++){
+    if ( string == numVids[i]) return console.log("yes")
+        else
+
+    return console.log("no")
+}
+
+}
+*/
