1a32cab61297c4eb6e912ad9dbecf1e180ee3ba5
Nyleec <nyleecp@gmail.com>
2025-04-13 09:44:54 -0400

    array of vids for auto

--- shows.js
+++ shows.js
@@ -1,9 +1,17 @@
 const shows = ["https://dhzuxxxhixmwv.cloudfront.net/videoplayback.mp4","https://dhzuxxxhixmwv.cloudfront.net/TheDrive-ftHOME-Resonance.mp4"];
 const numShows= shows.length;
+var src = "";
 
 //add function to create DOM modules on HTML page and place this loop inside
-for( let i =0; i<numShows; i++){
- const src = shows[i]
+function getShowURL([]){ 
 
-console.log(src)
+    for( let i =0; i<numShows; i++){
+        var src = shows[i];
+        console.log(src);
+    }
+return console.log("Success");
 }
+
+
+getShowURL(shows);
+
