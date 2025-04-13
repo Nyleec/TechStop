216cfbf60d903a2c94afc19d2920939cb1af18dd
Nyleec <nyleecp@gmail.com>
2025-04-13 09:27:38 -0400

    shows

---
+++ shows.js
@@ -0,0 +1,9 @@
+const shows = ["https://dhzuxxxhixmwv.cloudfront.net/videoplayback.mp4","https://dhzuxxxhixmwv.cloudfront.net/TheDrive-ftHOME-Resonance.mp4"];
+const numShows= shows.length;
+
+//add function to create DOM modules on HTML page and place this loop inside
+for( let i =0; i<numShows; i++){
+ const src = shows[i]
+
+console.log(src)
+}
